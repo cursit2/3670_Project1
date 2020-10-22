3ef862e20f284282c7d6c8527cf7791b71efc182
cursit2 <66439983+cursit2@users.noreply.github.com>
2020-10-22 11:39:19 -0400

    small update.
     waits for input. name changes

--- JobCreator.java
+++ JobCreator.java
@@ -1,12 +1,17 @@
 import java.net.*;
 import java.io.*;
-
-public class Client{
+import java.util.Scanner;
+//jobCreator creates job, connects to jobSeeker, gives job, waits for job to finish and come back.
+public class JobCreator{ //client
     public static void main(String[] args) throws IOException{
+        try{
         Socket s = new Socket("localhost", 4999);
 
         PrintWriter pr = new PrintWriter(s.getOutputStream());
-        pr.println("Is it working?");
+        Scanner sc = new Scanner(System.in);
+        String job = sc.nextLine();
+
+        pr.println(job);
         pr.flush();
 
         InputStreamReader in = new InputStreamReader(s.getInputStream());
@@ -14,5 +19,8 @@ public class Client{
 
         String str = bf.readLine();
         System.out.println("server: "+ str);
+        }catch(IOException e){
+            System.out.println("Connection failed. Abort");
+        }
     }
 }
--- JobServer.java
+++ JobServer.java
@@ -1,20 +1,21 @@
 import java.net.*;
 import java.io.*;
-
-public class Server{
+//job seeker waits for jobSeeker to connect, accepts job request, does job, reports back
+public class JobSeeker{ //server
     public static void main(String[] args) throws IOException{
         ServerSocket ss = new ServerSocket(4999);
         Socket s = ss.accept();
 
-        System.out.println("client connected");
+        System.out.println("JobCreator connected. Waiting for Job...");
         InputStreamReader in = new InputStreamReader(s.getInputStream());
         BufferedReader bf = new BufferedReader(in);
 
-        String str = bf.readLine();
-        System.out.println("client: "+ str);
+        String jobString = bf.readLine();
+        System.out.println("client: "+ jobString);
 
         PrintWriter pr = new PrintWriter(s.getOutputStream());
-        pr.println("Yes it is");
+        pr.println("Understood");
         pr.flush();
+
     }
 }
