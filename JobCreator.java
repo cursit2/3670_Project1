e7a7d5fc80e5933376464b0635a41cb5eff6f813
cursit2 <66439983+cursit2@users.noreply.github.com>
2020-10-22 12:25:05 -0400

    now does basic calculations
    job creator asks for calculation, job seeker does it and sends back

--- JobCreator.java
+++ JobCreator.java
@@ -2,23 +2,39 @@ import java.net.*;
 import java.io.*;
 import java.util.Scanner;
 //jobCreator creates job, connects to jobSeeker, gives job, waits for job to finish and come back.
+//create job with type, computational task    i.e multiply, 2, 5. addition,subtraction,division,multiplication
 public class JobCreator{ //client
     public static void main(String[] args) throws IOException{
+        String jobType;
+        int var1;
+        int var2;
         try{
         Socket s = new Socket("localhost", 4999);
 
         PrintWriter pr = new PrintWriter(s.getOutputStream());
         Scanner sc = new Scanner(System.in);
-        String job = sc.nextLine();
-
-        pr.println(job);
+        //get job from user. one of the 4 types
+        System.out.println("Enter job type: addition, subtraction, division, or multiplication");
+        jobType = sc.nextLine();
+        pr.println(jobType);
         pr.flush();
-
+        //get two ints from users
+        System.out.println("Enter 2 numbers");
+        var1 = sc.nextInt();
+        var2 = sc.nextInt();
+        pr.println(var1);
+        pr.flush();
+        pr.println(var2);
+        pr.flush();
+        //get response
         InputStreamReader in = new InputStreamReader(s.getInputStream());
         BufferedReader bf = new BufferedReader(in);
 
         String str = bf.readLine();
-        System.out.println("server: "+ str);
+        System.out.println("JobSeeker: "+ str+"\n");
+
+        str = bf.readLine();
+        System.out.println("JobSeeker: "+ str);
         }catch(IOException e){
             System.out.println("Connection failed. Abort");
         }
---
+++ JobSeeker.java
@@ -0,0 +1,49 @@
+import java.net.*;
+import java.io.*;
+//job seeker waits for jobSeeker to connect, accepts job request, does job, reports back
+public class JobSeeker{ //server
+    public static void main(String[] args) throws IOException{
+        String jobString;
+        int jobVar1;
+        int jobVar2;
+        boolean badJob = false;
+        double sum = 0;
+        ServerSocket ss = new ServerSocket(4999);
+        Socket s = ss.accept();
+
+        System.out.println("JobCreator connected. Waiting for Job...");
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        //get the job type from the creator
+        jobString = bf.readLine();
+        System.out.println("jobCreator: "+ jobString);
+
+        //get the 2 variables from creator
+        jobVar1 = Integer.parseInt(bf.readLine());
+        jobVar2 = Integer.parseInt(bf.readLine());
+        System.out.println("var1: "+jobVar1);
+        System.out.println("var2: "+jobVar2);
+
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.println("RECEIVED. Job Type: "+jobString + ", var1: "+ jobVar1 + ", var2: "+ jobVar2);
+        pr.flush();
+        //do the calculations
+        switch (jobString) {
+            case "addition", "Addition" -> sum = jobVar1 + jobVar2;
+            case "subtraction", "Subtraction" -> sum = jobVar1 - jobVar2;
+            case "division", "Division" -> sum = (double) (jobVar1 / jobVar2);
+            case "multiplication", "Multiplication" -> sum = jobVar1 * jobVar2;
+            default -> badJob = true;
+        }
+        if(!badJob) {
+            System.out.println("Job finished.");
+            pr.println("Job Complete. sum = " + sum);
+            pr.flush();
+        }else{
+            System.out.println("Job incomplete. Error.");
+            pr.println("Error. Unknown job type.");
+            pr.flush();
+        }
+    }
+}
